10729967b048452c4cabd68aa5b5cf3258f8f364
Sami McGuire <samimcguire@gmail.com>
2023-11-19 14:36:48 -0800

    connect redis

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import Left from './components/left';
 import Right from './components/right';
 import { useState } from 'react';
 
+function update(section, value) {
+    return new Promise((resolve, reject) => {
+        var url = `/update/${section}/${value}`;        
+        superagent
+            .get(url)
+            .end(function(err, res){
+                err ? reject(null) : resolve(res.body);
+            });
+    });
+}
+
+function read() {
+    return new Promise((resolve, reject) => {
+        var url = '/data';
+        superagent
+            .get(url)
+            .end(function(err, res){
+                err ? reject(null) : resolve(res.body);
+            });
+    });
+}
 
 function App(){
   const [data, setData]   = useState({header:0,left:0,article:0,right:0,footer:0});    
